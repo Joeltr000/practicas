504100b0959ced67eef663b1f0645b75920cb698
Joel <joelstudy00@gmail.com>
2025-10-04 22:22:24 -0600

    Se agrego bibliotecas y el int main

--- funciones.c
+++ funciones.c
@@ -1,9 +1,17 @@
+#include<stdio.h>
+#include<string.h>
 
 void swap (int *a, int *b);
 int longitudCadena ( char * cadena);
 void invertirArreglo (int *arr , int size);
 int cuentaPares (int *arr , int size);
 
+int main(){
+	return 0;
+}
+
 void main(){
 
+
 }
+
