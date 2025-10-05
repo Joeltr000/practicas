906c115202e61708b05430cbb29c2e3d487786df
Joel <joelstudy00@gmail.com>
2025-10-04 22:45:54 -0600

    se agregro la funcion de cambio nombrada swap

--- funciones.c
+++ funciones.c
@@ -7,11 +7,17 @@ void invertirArreglo (int *arr , int size);
 int cuentaPares (int *arr , int size);
 
 int main(){
-	return 0;
+	int x = 234;
+	int y = 109;
+	
+	printf("swap antes: x=%d, y=%d\n",x,y);
+	swap(&x,&y);
+	printf("swap despues: x=%d, y=%d\n",x,y);
 }
 
-void main(){
-
-
+void swap(int *a, int *b){
+	int cb = *a;
+	*a = *b;
+	*b = cb;
 }
 
