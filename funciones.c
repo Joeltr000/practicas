6f01919b80b3f875dadee4ea4c9bc90f1bf3dcb7
Joel <joelstudy00@gmail.com>
2025-10-05 15:09:53 -0600

    Se modifico un poco la estructura y se ordeno algunas cosas

--- funciones.c
+++ funciones.c
@@ -10,31 +10,27 @@ int cuentaPares (int *arr, int size);
 int main(){
 	int x = 234;
 	int y = 109;
-	int arreglo[] = {1, 2, 3, 4, 5};
-    int size = 5;
-    
+	int arreglo[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = 10;
+    int tamano = sizeof(arreglo) / sizeof(arreglo[0]); 
+    int numPares = cuentaPares(arreglo, tamano); 
+
 	printf("swap antes: x=%d, y=%d\n",x,y);
 	swap(&x,&y);
 	printf("swap despues: x=%d, y=%d\n",x,y);
 	
 	char texto[] = "Esta es una practica de la carrera de IIS";
-	printf("Esta es una practica de la carrera de IIS\n");
+	printf("El mensaje es: Esta es una practica de la carrera de IIS\n");
     printf("Longitud: %d\n", longitudCadena(texto));
     
-    printf("Antes:\n");
+    printf("Antes\n");
     imprimirArreglo(arreglo, size);
-    
     invertirArreglo(arreglo, size);
-    
-    printf("Despues:\n");
+    printf("Despues\n");
     imprimirArreglo(arreglo, size);
     
-    int miMatriz[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int tamano = sizeof(miMatriz) / sizeof(miMatriz[0]); // Calcula el tamaño de la matriz
-    
-    int numPares = cuentaPares(miMatriz, tamano); // Llama a la función
-
-    printf("El número de elementos pares en la matriz es: %d\n", numPares); // Imprime el resultado
+   
+    printf("El numero de elementos pares en la matriz es: %d\n", numPares);
     
     return 0;
 }
@@ -72,7 +68,7 @@ void invertirArreglo(int *arr, int size) {
 
 
 void imprimirArreglo(int *arr, int size) {
-    printf("Arreglo: ");
+    printf("El arreglo: ");
     for (int o = 0; o < size; o++) {
         printf("%d ", arr[o]);
     }
@@ -80,17 +76,17 @@ void imprimirArreglo(int *arr, int size) {
 }
 
 int cuentaPares (int *arr, int size){
-	    int contador = 0; // Inicializa el contador de números pares
-    int *puntero_fin = arr + size; // Puntero al final del array para el bucle
+	int contador = 0;
+    int *puntero_fin = arr + size; 
 
-    // Recorre la matriz usando aritmética de punteros
+  
     for (int *puntero_actual = arr; puntero_actual < puntero_fin; puntero_actual++) {
-        // Verifica si el elemento actual es par
+        
         if (*puntero_actual % 2 == 0) {
-            contador++; // Incrementa el contador si es par
+            contador++; 
         }
     }
-    return contador; // Devuelve el total de números pares encontrados
+    return contador; 
 }
 
 
